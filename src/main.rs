5acf2295e8af2610c88e5c901a26f8949e76ec0d
Akio-m <ahigachika@gmail.com>
2020-07-08 01:54:23 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use serde_json::json;
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut app = tide::new();
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.at("/ping").get(|_| async { Ok(json!({"ok" : "pong"})) });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}

3b548934c3d6ccf250c666473967f650504aa01f
Akio-m <ahigachika@gmail.com>
2020-08-17 23:20:11 +0900

    set logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use serde_json::json;
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
+    tide::log::start();
     let mut app = tide::new();
     app.at("/").get(|_| async { Ok("Hello, world!") });
     app.at("/ping").get(|_| async { Ok(json!({"ok" : "pong"})) });

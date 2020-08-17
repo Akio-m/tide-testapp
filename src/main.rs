09a8bedc3b9cd4d78b6ee3fa77312a6e460295b0
Akio-m <ahigachika@gmail.com>
2020-08-18 00:12:13 +0900

    extract function and create response function.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use serde_json::json;
+use serde::{Deserialize, Serialize};
+use tide::Request;
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     tide::log::start();
     let mut app = tide::new();
-    app.at("/").get(|_| async { Ok("Hello, world!") });
-    app.at("/ping").get(|_| async { Ok(json!({"ok" : "pong"})) });
+    app.at("/ping").get(ping);
+    app.at("/ping").post(post2response);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
+}
+
+async fn ping(_request: Request<()>) -> tide::Result<serde_json::Value> {
+    Ok(json!({"ok" : "pong"}))
+}
+
+async fn post2response(mut _request: Request<()>) -> tide::Result<serde_json::Value> {
+    let body: Ping = _request.body_json().await?;
+    Ok(json!(&body))
+}
+
+#[derive(Deserialize, Serialize)]
+struct Ping {
+    value: String,
 }
